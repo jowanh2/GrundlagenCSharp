8c14618c62d19f3f03367edf5c4f971b6c40304c
Jowan Hamko <jowan.h2@gso.schule.koeln>
2022-02-09 10:14:03 +0100

    Projektdateien hinzufügen.

---
+++ Finanztabelle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Finanztabelle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
