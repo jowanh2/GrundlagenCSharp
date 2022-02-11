0cf43c475188b23ceef00a7525a91c4e5a362761
Jowan Hamko <jowan.h2@gso.schule.koeln>
2022-02-11 11:49:48 +0100

    gaming

--- Finanztabelle/Program.cs
+++ Finanztabelle/Program.cs
@@ -6,7 +6,65 @@ namespace Finanztabelle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         Console.WriteLine("{0,3} {1,10} {2,8} {3,10} {4,8}","Jahr","Guthaben","Zinsen","k.zinsen","bilanz");
+
+
+
+            Console.WriteLine(" ");
+
+
+
+            double k = 0, j = 0, P = 0;
+            double kz = 0, b = 0;
+
+
+
+
+            Console.WriteLine("Geben sie ihre Kapital ein");
+            k = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+            Console.WriteLine("Geben sie ihre Prozentsatz ein");
+            P = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Wieviele Jahre soll des dauern?");
+            j = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("{0,3} {1,10} {2,8} {3,10} {4,8}", "Jahr", "Guthaben", "Zinsen", "k.zinsen", "bilanz");
+
+
+
+            for (int i = 0; i <= 43; i++)
+            {
+
+
+
+               Console.Write("-");
+            }
+            Console.WriteLine("");
+            
+             int Jahr; 
+            for (Jahr<=j;Jahr++)
+            {
+            kz = kz + k * (P / 100);
+            b = b + k;
+
+
+            Console.WriteLine("{0,3}| {1,9:F2}Euro| {2,6:F2}Euro| {3,8:F2}Euro| {4,6:F2}Euro", Jahr, k, k* (P/100), kz, b);
+            k = k + (k * (P / 100));
+            }
+            Console.ReadKey();
+
+
+
+            }
         }
     }
 }
