8055dd827a3cc53363ead24e8afec19235e47ca8
Jowan Hamko <jowan.h2@gso.schule.koeln>
2022-02-11 12:01:34 +0100

    aomdfad

--- Finanztabelle/Program.cs
+++ Finanztabelle/Program.cs
@@ -8,11 +8,7 @@ namespace Finanztabelle
         {
          Console.WriteLine("{0,3} {1,10} {2,8} {3,10} {4,8}","Jahr","Guthaben","Zinsen","k.zinsen","bilanz");
 
-
-
             Console.WriteLine(" ");
-
-
 
             double k = 0, j = 0, P = 0;
             double kz = 0, b = 0;
@@ -41,30 +37,31 @@ namespace Finanztabelle
 
 
 
-            for (int i = 0; i <= 43; i++)
+            for (int i = 0; i <= 60; i++)
             {
-
-
-
                Console.Write("-");
             }
             Console.WriteLine("");
-            
-             int Jahr; 
-            for (Jahr<=j;Jahr++)
+            j--;
+            for (int J = 0; J<=j;J++)
             {
             kz = kz + k * (P / 100);
             b = b + k;
 
 
-            Console.WriteLine("{0,3}| {1,9:F2}Euro| {2,6:F2}Euro| {3,8:F2}Euro| {4,6:F2}Euro", Jahr, k, k* (P/100), kz, b);
+            Console.WriteLine("{0,3}| {1,9:F2}Euro| {2,6:F2}Euro| {3,8:F2}Euro| {4,6:F2}Euro", J, k, k* (P/100), kz, b);
             k = k + (k * (P / 100));
             }
-            Console.ReadKey();
+          
 
 
-
+            for (int i = 0; i <= 60; i++)
+            {
+             Console.Write("-");
             }
         }
+
+
+        }
     }
-}
+
