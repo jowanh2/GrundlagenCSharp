b37f0eee3293f4c2b7d634379707e37d906260cc
Jowan Hamko <jowan.h2@gso.schule.koeln>
2022-03-11 12:43:08 +0100

    Aufgabe fertig

--- Array_bubblesort/Program.cs
+++ Array_bubblesort/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-
-namespace Array_bubblesort
+namespace SortingExample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] number = { 45, 15, 4, 10 };
+            bool var = true;
+            int temp;
+            int numLength = number.Length;
+
+
+            Console.WriteLine("Arrays bevor Sortierung\n");
+            Console.WriteLine(number[0]);
+            Console.WriteLine(number[1]);
+            Console.WriteLine(number[2]);
+            Console.WriteLine(number[3]);
+
+
+
+            for (int i = 1; (i <= (numLength - 1)) && var; i++)
+            {
+                var = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (number[j + 1] > number[i])
+                    {
+                        temp = number[i];
+                        number[i] = number[j + 1];
+                        number[i + 1] = temp;
+                        var = true;
+                    }
+                }
+            }
+
+            Console.WriteLine("Arrays nach Sortierung\n");
+
+
+            foreach (int num in number)
+            {
+                Console.WriteLine("{0}", num);
+            }
+            Console.Read();
         }
     }
 }
