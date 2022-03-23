a1c0335a3d6a8ba6869f96b71bd254eb93170ca4
Jowan Hamko <jowan.h2@gso.schule.koeln>
2022-03-23 12:42:44 +0100

    Aufgabe 1

---
+++ Methoden/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Methoden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string name = "";
+            Console.WriteLine("Bitte geben sie ihre Text ein");
+            name = (Console.ReadLine());
+
+  
+            Console.WriteLine("Bitte geben sie ihre durchläufe der Anzahl");
+            int durchlauf=Convert.ToInt32(Console.ReadLine());
+
+
+            int anzahl_zeichen = 0;
+
+            anzahl_zeichen = ausgabeTextMalX(name, durchlauf);
+            Console.Write("\nAnzahlder Buchstaben: {0}", anzahl_zeichen);
+        }
+
+        static int ausgabeTextMalX( string text, int n)
+        {
+            int anz_zeichen_text = text.Length;
+            int anz_alle_zeichen = anz_zeichen_text * n;
+
+
+            for (int i = 0; i < anz_zeichen_text; i++) 
+            {
+                Console.WriteLine(text);
+            }
+            return anz_alle_zeichen;
+        }
+    
+    }
+
+
+}
